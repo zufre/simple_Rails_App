8a59fa2e04514d65bbab034971e390ed0c14f095
User <user@Mac.local>
2018-04-29 16:42:21 +1000

    fixed some bugs and add blog in navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,25 @@
 class PagesController < ApplicationController
   def index
-
+        @title = 'Welcome'
+        @content = 'Welcome to the homepage'
   end
 
   def about
-
+        @title = 'About'
+        @content = 'Welcome to the About page'
+  
   end
 
-  def servises
-
+  def services
+        @title = 'Services'
+        @content = 'Welcome to the Services page'
+        @services = ["web Design", "Web Development", "SEO"]
   end
+
 
   def contact
-
+        @title ='Contact'
+        @content = 'Welcome to the Contact page'
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#index', as: 'home'
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about', as: 'about'
   get 'services' => 'pages#services', as:'services'
   get 'contact' => 'pages#contact', as:'contact'
+  
 end
