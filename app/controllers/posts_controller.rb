4a137d80b142bc646fcb9750cdfc55447b8ea6cc
User <user@Mac.local>
2018-04-30 12:06:46 +1000

    added data validation and posts listing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
    # render plain: params[:post].inspect
    @post = Post.new(post_params)
-   @post.save
-   redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+     render 'new'
+   end
   end
 
   def edit
